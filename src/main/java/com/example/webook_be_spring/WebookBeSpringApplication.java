65264e7a79a38990597fe6d69da5d0f745521c41
duongtranngoc <duongtn0306@gmail.com>
2023-12-17 18:01:10 +0700

    created basic app with spring-boot@3.2.0

---
+++ src/main/java/com/example/webook_be_spring/WebookBeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.webook_be_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebookBeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebookBeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webook_be_spring/WebookBeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webook_be_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebookBeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
